2b6e62b75c8b4e268fdc3905e84f1e4fa167bd64
Zulfan Syafiqi <zulfansyafiqi2001@gmail.com>
2026-04-11 15:01:18 +0700

    Initial commit SPK Profile Matching Laravel

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,132 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <h2> <b>SPK</b> </h2>
+        </div>
+        <div class="sidebar-brand-text mx-3">Profile Matching<sup></sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="/dashboard">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Data Master -->
+    <div class="sidebar-heading">
+        Data Master
+    </div>
+
+    <!-- Alternatif -->
+    <li class="nav-item">
+        <a class="nav-link" href="/alternatif">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Data Alternatif</span>
+        </a>
+    </li>
+
+    <!-- Kriteria -->
+    <li class="nav-item">
+        <a class="nav-link" href="/kriteria">
+            <i class="fas fa-fw fa-list"></i>
+            <span>Data Kriteria</span>
+        </a>
+    </li>
+
+    <!-- Sub Kriteria -->
+    <li class="nav-item">
+        <a class="nav-link" href="/subkriteria">
+            <i class="fas fa-fw fa-sitemap"></i>
+            <span>Sub Kriteria</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <!-- Penilaian -->
+    <div class="sidebar-heading">
+        Penilaian
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/penilaian">
+            <i class="fas fa-fw fa-edit"></i>
+            <span>Input Penilaian</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <!-- Perhitungan -->
+    <div class="sidebar-heading">
+        Perhitungan
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/perhitungan">
+            <i class="fas fa-fw fa-calculator"></i>
+            <span>Profile Matching</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <!-- Hasil -->
+    <div class="sidebar-heading">
+        Hasil
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/ranking">
+            <i class="fas fa-fw fa-trophy"></i>
+            <span>Hasil Ranking</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <!-- Laporan -->
+    <div class="sidebar-heading">
+        Laporan
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/laporan">
+            <i class="fas fa-fw fa-file-alt"></i>
+            <span>Laporan</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <!-- Pengaturan -->
+    <div class="sidebar-heading">
+        Pengaturan
+    </div>
+
+    <!-- User -->
+    <li class="nav-item">
+        <a class="nav-link" href="/user">
+            <i class="fas fa-fw fa-user-cog"></i>
+            <span>Manajemen User</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
