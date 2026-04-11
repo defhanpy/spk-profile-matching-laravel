25db0b1b2dff66164ddceb5950ab1a9d9790d781
Zulfan Syafiqi <zulfansyafiqi2001@gmail.com>
2026-04-11 16:55:57 +0700

    fix conflict

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -23,12 +23,4 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-<<<<<<< HEAD
-    <link href="{{ asset('../css/sb-admin-2.min.css') }}" rel="stylesheet">
-=======
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
->>>>>>> 2b6e62b75c8b4e268fdc3905e84f1e4fa167bd64
-
 </head>
