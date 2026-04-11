37e448707d7a890a7e08d5a676de8eca1b165db5
Zulfan Syafiqi <zulfansyafiqi2001@gmail.com>
2026-04-11 15:01:18 +0700

    memperbaharui menu

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('components.header')
+
+<body id="page-top">
+
+<div id="wrapper">
+
+    {{-- Sidebar --}}
+    @include('components.sidebar')
+
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+
+            {{-- Topbar --}}
+            @include('components.topbar')
+
+            {{-- CONTENT --}}
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+
+        </div>
+
+        {{-- Footer --}}
+        @include('components.footer')
+    </div>
+
+</div>
+
+{{-- Scripts --}}
+@include('components.scripts')
+
+</body>
+</html>
